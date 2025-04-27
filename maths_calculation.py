042606659e8910e7552bbabcc7a9299a226c69ca
Khan-anam01 <paulanam4@outlook.com>
2025-04-28 00:28:51 +0300

    initial commit

--- maths_calculation.py
+++ maths_calculation.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 # while True:
@@ -68,4 +69,42 @@ while True:
         break
 
 
+=======
+choice = "YES"
+while choice == "YES":
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operand = input("Enter the operand(*, /, +, -, %): ")
+        result = 0
+    except ValueError:
+        print("Invalid input!")
+        break
+
+    if (operand == "*"):
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif (operand == "/"):
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("a number can be divised by 0")
+    elif (operand == "+"):
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif (operand == "-"):
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif (operand == "%"):
+        if num2 != 0:
+            result = num1 % num2
+            print(f"{num1} % {num2} = {result}")
+        else:
+            print("a number can be divised by 0")
+    else:
+        print("Invalid operand!")
+    
+    choice = input("Do you want to do another calculation?(yes/no): ").upper()
+>>>>>>> 28c04b7c40696ed9cc7f5864ae73626bc072a72e
 
